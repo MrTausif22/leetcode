04197a57c9e5d781e3f3f870fb7eeed10193df02
Tausif Alam <tausiffaizan22@gmail.com>
2025-12-31 18:07:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (12.97%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,13 +1,28 @@
-class Solution {
-    public int fib(int n) {
-        if (n <= 1){
-            return n;
+// class Solution {
+//     public int fib(int n) {
+//         if (n <= 1){
+//             return n;
+//         }
+//         int fibonacci = fib(n-1) + fib(n-2);
+//         return fibonacci;
+//     }
+// }
 
+class Solution {
+    public int fib(int n){
+        if (n<= 1){
+            return n;
         }
 
+        int n1 = 0;
+        int n2 = 1;
 
-        int fibonacci = fib(n-1) + fib(n-2);
-        return fibonacci;
-        
+        for(int i = 2 ; i <= n ; i++){
+            int n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+        return n2;
     }
 }
+
