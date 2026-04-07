fa484349f6a64a03e2ba8639ed7f090567576ab5
Tausif Alam <tausiffaizan22@gmail.com>
2026-04-07 14:31:44 +0530

    Time: 1 ms (98.09%) | Memory: 42.7 MB (52.42%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,21 +1,21 @@
-// class Solution {
-//     public int addDigits(int num) {
-//         if (num == 0) return 0;
-//         return 1 + (num - 1) % 9;
-//     }
-// }
-
-
 class Solution {
     public int addDigits(int num) {
-        while (num >= 10) {   // repeat until single digit
-            int sum = 0;
-            while (num > 0) {
-                sum += num % 10;  // get last digit
-                num /= 10;        // remove last digit
-            }
-            num = sum;
-        }
-        return num;
+        if (num == 0) return 0;
+        return 1 + (num - 1) % 9;
     }
 }
+
+
+// class Solution {
+//     public int addDigits(int num) {
+//         while (num >= 10) {   // repeat until single digit
+//             int sum = 0;
+//             while (num > 0) {
+//                 sum += num % 10;  // get last digit
+//                 num /= 10;        // remove last digit
+//             }
+//             num = sum;
+//         }
+//         return num;
+//     }
+// }
