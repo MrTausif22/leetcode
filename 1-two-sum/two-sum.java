97767dc6fdcf47615fb4ad711a320288d0ab4585
Tausif Alam <tausiffaizan22@gmail.com>
2026-05-03 11:14:27 +0530

    Time: 2 ms (99.32%) | Memory: 47.3 MB (23.70%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,15 +1,15 @@
 // import java.util.HashMap;
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i, j};
-                }
-            }
-        }
-        return new int[]{};
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         for (int i = 0; i < nums.length; i++) {
+//             for(int j=i+1; j<nums.length; j++){
+//                 if(nums[i]+nums[j]==target){
+//                     return new int[]{i, j};
+//                 }
+//             }
+//         }
+//         return new int[]{};
             
 
     // public static void main(String[] args) {
@@ -19,5 +19,26 @@ class Solution {
     //     int result[] = sol.twoSum(arr, target);
 
     //     System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+//     }
+// }
+
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            
+            map.put(nums[i], i);
+        }
+        
+        return new int[]{};
     }
 }
