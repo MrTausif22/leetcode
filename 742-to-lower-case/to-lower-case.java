e6463529a470d40d8d2dc5feeff8cd2047e339d4
Tausif Alam <tausiffaizan22@gmail.com>
2026-05-06 09:56:57 +0530

    Time: 1 ms (26.79%) | Memory: 43.1 MB (19.04%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -10,15 +10,24 @@ class Solution {
     // }
     // //     return s.toLowerCase(); 
 
-    String result = "";
-    for(int i = 0; i< s.length(); i++){
-        if('A' <= s.charAt(i) && 'Z' >= s.charAt(i)){
-            result += (char) (s.charAt(i)+ 32);
-        }else{
-            result += s.charAt(i);
-        }
-    }
-    return result;
+    // String result = "";
+    // for(int i = 0; i< s.length(); i++){
+    //     if('A' <= s.charAt(i) && 'Z' >= s.charAt(i)){
+    //         result += (char) (s.charAt(i)+ 32);
+    //     }else{
+    //         result += s.charAt(i);
+    //     }
+    // }
+    // return result;
 
+    StringBuilder sb = new StringBuilder();
+
+    for(char c : s.toCharArray()){
+        if(c >= 65 && c <= 90){
+            c = (char) (c+32);
+        }
+        sb.append(c);
+    }
+        return sb.toString();
     }
 }
