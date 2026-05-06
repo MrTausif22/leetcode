e12fa5518f6c84a77f0c8c0334dc422bdf07c28b
Tausif Alam <tausiffaizan22@gmail.com>
2026-05-06 09:50:33 +0530

    Time: 3 ms (10.11%) | Memory: 43.6 MB (5.58%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,14 +1,24 @@
 class Solution {
     public String toLowerCase(String s) {
-    //     return s.toLowerCase();  
+    // char[] c = s.toCharArray();
+    // for(int i = 0; i< c.length; i++){
+    //     if(c[i] >= 65 && c[i] <= 90){
+    //         c[i] = (char) (c[i] +32);
+    //     }
+    // }
+    // return String.valueOf(c);
+    // }
+    // //     return s.toLowerCase(); 
 
-    char[] c = s.toCharArray();
-    for(int i = 0; i< c.length; i++){
-        if(c[i] >= 65 && c[i] <= 90){
-            c[i] = (char) (c[i] +32);
+    String result = "";
+    for(int i = 0; i< s.length(); i++){
+        if('A' <= s.charAt(i) && 'Z' >= s.charAt(i)){
+            result += (char) (s.charAt(i)+ 32);
+        }else{
+            result += s.charAt(i);
         }
     }
+    return result;
 
-    return String.valueOf(c);
     }
 }
