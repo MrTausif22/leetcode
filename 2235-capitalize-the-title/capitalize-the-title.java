d47e2f56110782708a8dcce29fc752fd550a119c
Tausif Alam <tausiffaizan22@gmail.com>
2026-05-08 11:03:16 +0530

    Time: 5 ms (42.66%) | Memory: 43.7 MB (70.10%) - LeetSync

--- 2235-capitalize-the-title/capitalize-the-title.java
+++ 2235-capitalize-the-title/capitalize-the-title.java
@@ -1,14 +1,31 @@
+// class Solution {
+//     public String capitalizeTitle(String title) {
+//         String [] words = title.split(" ");
+//         for(int i = 0; i< words.length; i++){
+//             String w = words[i].toLowerCase();
+
+//             if(w.length()> 2){
+//                 w = Character.toUpperCase(w.charAt(0)) + w.substring(1);
+//             }
+//             words[i] = w;
+//         }
+//         return String.join(" ",words);
+//     }
+// }
+
 class Solution {
     public String capitalizeTitle(String title) {
-        String [] words = title.split(" ");
-        for(int i = 0; i< words.length; i++){
-            String w = words[i].toLowerCase();
 
-            if(w.length()> 2){
-                w = Character.toUpperCase(w.charAt(0)) + w.substring(1);
+        String[] words = title.toLowerCase().split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+
+            if (words[i].length() > 2) {
+                words[i] = Character.toUpperCase(words[i].charAt(0))
+                        + words[i].substring(1);
             }
-            words[i] = w;
         }
-        return String.join(" ",words);
+
+        return String.join(" ", words);
     }
 }
