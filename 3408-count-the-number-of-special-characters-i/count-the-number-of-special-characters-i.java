e095a3d4d9be1be347ac9001b40a554bcf046960
Tausif Alam <tausiffaizan22@gmail.com>
2026-05-10 22:48:11 +0530

    Time: 1 ms (100.00%) | Memory: 43 MB (99.42%) - LeetSync

---
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -0,0 +1,43 @@
+// class Solution {
+//     public int numberOfSpecialChars(String word) {
+//         int count = 0;
+//         char [] w = word.toCharArray();
+//         for(int i=0; i< word.length(); i++){
+//             for(int j = i+1; j <word.length(); j++){
+//                 if(w[i] == (char)(w[j]+32)){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+class Solution {
+    public int numberOfSpecialChars(String word) {
+
+        boolean[] lower = new boolean[26];
+        boolean[] upper = new boolean[26];
+
+        // Mark lowercase and uppercase
+        for (char ch : word.toCharArray()) {
+
+            if (Character.isLowerCase(ch)) {
+                lower[ch - 'a'] = true;
+            } else {
+                upper[ch - 'A'] = true;
+            }
+        }
+
+        // Count special characters
+        int count = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] && upper[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
