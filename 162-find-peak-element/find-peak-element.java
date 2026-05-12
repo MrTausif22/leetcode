20e3480e1c12f43e6daa5d3bc77bd6fe67ba9f9a
Tausif Alam <tausiffaizan22@gmail.com>
2026-05-12 22:24:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (86.55%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,42 +1,55 @@
-// class Solution {
-//     public int findPeakElement(int[] nums) {
-//        int left =0;
-//        int right = nums.length-1; 
-
-//        while(left < right){
-
-//         int mid = left + (right - left)/2;
-//         if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
-//             return mid;
-//         }
-//         else if(nums[mid] < nums[mid -1]){
-//             right = mid -1;
-//         }
-//         else{
-//             left = mid+1;
-//         }
-//        }
-       
-//     }
-// }
-
 class Solution {
     public int findPeakElement(int[] nums) {
-
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left < right) {
-
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] > nums[mid + 1]) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
+        int n = nums.length-1;
+       int left =0;
+       int right = n; 
+       
+        if (nums.length == 1) {
+            return 0;
         }
+       if(nums[0] > nums[1]){
+        return 0;
+       }
+       if(nums[n] > nums[n-1]){
+        return n;
+       }
 
-        return left;
+       while(left < right){
+
+        int mid = left + (right - left)/2;
+        if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+            return mid;
+        }
+        else if(nums[mid] < nums[mid -1]){
+            right = mid;
+        }
+        else{
+            left = mid+1;
+        }
+        
+       }
+       return -1;
+       
     }
 }
+
+// class Solution {
+//     public int findPeakElement(int[] nums) {
+
+//         int left = 0;
+//         int right = nums.length - 1;
+
+//         while (left < right) {
+
+//             int mid = left + (right - left) / 2;
+
+//             if (nums[mid] > nums[mid + 1]) {
+//                 right = mid;
+//             } else {
+//                 left = mid + 1;
+//             }
+//         }
+
+//         return left;
+//     }
+// }
