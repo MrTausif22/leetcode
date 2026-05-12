cf41fefa23abdb6d67ba8f29b65a649e452f40f5
Tausif Alam <tausiffaizan22@gmail.com>
2026-05-12 22:00:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (18.61%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,42 @@
+// class Solution {
+//     public int findPeakElement(int[] nums) {
+//        int left =0;
+//        int right = nums.length-1; 
+
+//        while(left < right){
+
+//         int mid = left + (right - left)/2;
+//         if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+//             return mid;
+//         }
+//         else if(nums[mid] < nums[mid -1]){
+//             right = mid -1;
+//         }
+//         else{
+//             left = mid+1;
+//         }
+//        }
+       
+//     }
+// }
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
