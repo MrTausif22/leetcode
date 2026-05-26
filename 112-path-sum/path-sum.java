105f42b6bf75d181afdd651ec9ff6423e9647512
Tausif Alam <tausiffaizan22@gmail.com>
2026-05-26 15:15:16 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (90.81%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -13,45 +13,12 @@
  *     }
  * }
  */
-class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-
-        if(root == null){
-            return false;
-        }
-        Stack<TreeNode> path = new Stack<>();
-        Stack<Integer> sumPath = new Stack<>();
-
-        path.push(root);
-        sumPath.push(root.val);
-
-        while(!path.isEmpty()){
-            TreeNode temp = path.pop();
-            int tempVal = sumPath.pop();
-
-            if(temp.left == null && temp.right == null && tempVal == targetSum){
-                return true;
-            }
-            if(temp.left != null){
-                path.push(temp.left);
-                sumPath.push(temp.left.val + tempVal);
-            }
-            if(temp.right != null){
-                path.push(temp.right);
-                sumPath.push(temp.right.val + tempVal);
-            }
-        }
-        return false;
-    }
-}
-
 // class Solution {
 //     public boolean hasPathSum(TreeNode root, int targetSum) {
 
 //         if(root == null){
 //             return false;
 //         }
-
 //         Stack<TreeNode> path = new Stack<>();
 //         Stack<Integer> sumPath = new Stack<>();
 
@@ -59,25 +26,53 @@ class Solution {
 //         sumPath.push(root.val);
 
 //         while(!path.isEmpty()){
-
 //             TreeNode temp = path.pop();
 //             int tempVal = sumPath.pop();
 
 //             if(temp.left == null && temp.right == null && tempVal == targetSum){
 //                 return true;
 //             }
-
 //             if(temp.left != null){
 //                 path.push(temp.left);
 //                 sumPath.push(temp.left.val + tempVal);
 //             }
-
 //             if(temp.right != null){
 //                 path.push(temp.right);
 //                 sumPath.push(temp.right.val + tempVal);
 //             }
 //         }
-
 //         return false;
 //     }
 // }
+
+// class Solution {
+//     public boolean hasPathSum(TreeNode root, int targetSum) {
+
+//         if(root == null){
+//             return false;
+//         }
+//         // int currSum = 0;
+//         if(root.left == null && root.right == null ){
+//             return root.val == targetSum ;
+//         }
+//         return hasPathSum(root.left ,targetSum - root.val) || hasPathSum(root.righttargetSum - root.val);
+
+//     }
+// }
+
+class Solution {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if(root == null){
+            return false;
+        }
+
+        if(root.left == null && root.right == null){
+            return targetSum == root.val;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val)
+            || hasPathSum(root.right, targetSum - root.val);
+    }
+}
